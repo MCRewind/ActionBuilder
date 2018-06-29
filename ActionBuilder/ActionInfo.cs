03624221208bf6403b5e1c1ad45a94cdcd61a7c0
Rewind <sploing1@gmail.com>
2018-06-28 23:01:00 -0700

    Added JSON unit test (it doesnt work) and switched to JSON.net for json stuff
    I hate equality why is it bad why cant assert just work, also added fluentasserts

--- ActionBuilder/ActionInfo.cs
+++ ActionBuilder/ActionInfo.cs
@@ -5,46 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
 using System.Runtime.Serialization;
+using System.Windows;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace ActionBuilder
 {
-    [DataContract]
-    internal class ActionInfo
+    public class ActionInfo : IEquatable<ActionInfo>
     {
         enum FrameType { Startup, Active, Recovery, Buffer }
 
-        [DataMember]
         public string Name;
 
-        [DataMember]
+        [JsonConverter(typeof(StringEnumConverter))]
         private List<FrameType> _frames; 
 
-        public int FrameCount { get => _frames.Count; }
+        public int FrameCount => _frames.Count;
 
-        [DataMember]
         public Types.ActionType Type;
 
-        [DataMember]
         private Vector2 _infinite;
         public float InfiniteRangeMin { get => _infinite.X; set => _infinite.X = value; }
         public float InfiniteRangeMax { get => _infinite.Y; set => _infinite.Y = value; }
-
+        
         public ActionInfo()
         {
             _frames = new List<FrameType> { FrameType.Startup };
             _infinite = new Vector2(-1, -1);
-            Hitboxes = new List<List<Box>>();
-            Hurtboxes = new List<List<Box>>();
         }
 
-        [DataContract]
-        public class Box
+        public class Box : IEquatable<Box>
         {
-            [DataMember]
-            public Vector2 KnockbackAngle;
-            [DataMember]
+           public Vector2 KnockbackAngle;
             public double Damage, KnockbackStrength;
-            [DataMember]
             public int Lifespan, X, Y, Width, Height;
 
             public Box(int x, int y, int width, int height, double damage, double knockbackStrength, Vector2 knockbackAngle, int lifespan)
@@ -63,9 +56,54 @@ namespace ActionBuilder
 
             public void SetPos(int x, int y) { this.X = x; this.Y = y; }
             public void SetDims(int w, int h) { this.Width = w; this.Height = h; }
+
+            public bool Equals(Box other)
+            {
+                if (other is null) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return KnockbackAngle.Equals(other.KnockbackAngle) 
+                       && Damage.Equals(other.Damage) 
+                       && KnockbackStrength.Equals(other.KnockbackStrength) 
+                       && Lifespan == other.Lifespan 
+                       && X == other.X && Y == other.Y 
+                       && Width == other.Width && Height == other.Height;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is null) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals(obj as Box);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    var hashCode = KnockbackAngle.GetHashCode();
+                    hashCode = (hashCode * 397) ^ Damage.GetHashCode();
+                    hashCode = (hashCode * 397) ^ KnockbackStrength.GetHashCode();
+                    hashCode = (hashCode * 397) ^ Lifespan;
+                    hashCode = (hashCode * 397) ^ X;
+                    hashCode = (hashCode * 397) ^ Y;
+                    hashCode = (hashCode * 397) ^ Width;
+                    hashCode = (hashCode * 397) ^ Height;
+                    return hashCode;
+                }
+            }
+
+            public static bool operator ==(Box left, Box right)
+            {
+                return Equals(left, right);
+            }
+
+            public static bool operator !=(Box left, Box right)
+            {
+                return !Equals(left, right);
+            }
         }
 
-        [DataMember]
         public List<List<Box>> Hitboxes, Hurtboxes;
 
         public void InsertFrame(int index)
@@ -92,6 +130,49 @@ namespace ActionBuilder
                 _frames.RemoveAt(0);
         }
 
+        public bool Equals(ActionInfo other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Name, other.Name) 
+                   && Equals(_frames, other._frames) 
+                   && Type == other.Type 
+                   && _infinite.Equals(other._infinite) 
+                   && Hitboxes.SequenceEqual(other.Hitboxes)
+                   && Hurtboxes.SequenceEqual(other.Hurtboxes);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals(obj as ActionInfo);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (_frames != null ? _frames.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (int) Type;
+                hashCode = (hashCode * 397) ^ _infinite.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Hitboxes != null ? Hitboxes.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Hurtboxes != null ? Hurtboxes.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(ActionInfo left, ActionInfo right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ActionInfo left, ActionInfo right)
+        {
+            return !Equals(left, right);
+        }
     }
 
 }
---
+++ ActionBuilderTests/JSONTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Numerics;
+using ActionBuilder;
+using FluentAssertions;
+using FluentAssertions.Common;
+using FluentAssertions.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace ActionBuilderTests
+{
+    [TestClass]
+    public class JsonTests
+    {
+        [TestMethod]
+        public void ActionSerializeDeserializeTest()
+        {
+            var action = new ActionInfo()
+            {
+                Name = "Test Action",
+                Type = Types.ActionType.JAB,
+                Hitboxes = new List<List<ActionInfo.Box>>()
+                {
+                    // frame 0
+                    new List<ActionInfo.Box>()
+                    {
+                        // hitbox 0
+                        new ActionInfo.Box()
+                        {
+                            X = 0,
+                            Y = 0,
+                            Width = 4,
+                            Height = 4,
+                            Damage = 10,
+                            KnockbackAngle = new Vector2(0),
+                            KnockbackStrength = 5,
+                            Lifespan = 1
+                        }
+                    }
+                },
+                Hurtboxes = new List<List<ActionInfo.Box>>()
+                {
+                    // frame 0
+                    new List<ActionInfo.Box>()
+                    {
+                        // hurtbox 0
+                        new ActionInfo.Box()
+                        {
+                            X = 10,
+                            Y = 10,
+                            Width = 10,
+                            Height = 10,
+                            Damage = 0,
+                            KnockbackAngle = new Vector2(0),
+                            KnockbackStrength = 0,
+                            Lifespan = 1
+                        }
+                    }
+                }
+            };
+
+            var serializedAction = JsonConvert.SerializeObject(action, Formatting.Indented);
+            var deserializedAction = JsonConvert.DeserializeObject<ActionInfo>(serializedAction);
+
+            deserializedAction.Should().Be(action);
+
+        }
+    }
+}
